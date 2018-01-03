03cafde7a55376ed8908b1699abaa447bb8225e5
sergitab94 <16195794+sergitab94@users.noreply.github.com>
2018-01-03 02:39:47 +0100

    Cambios en la lógia del programa
    Se añade bloqueo a las partes prohibidas del programa

--- Animacion/Proyecto/scripts/Arena.lua
+++ Animacion/Proyecto/scripts/Arena.lua
@@ -1,5 +1,11 @@
 -- Obtener Objeto
 Jugador = getObject("Jugador")
+Cam = getObject("Camara")
+P1 = getObject("Prohibido1")
+P2 = getObject("Prohibido2")
+P3 = getObject("Prohibido3")
+cont = 0
+fot = 0
 
 -- Actualización de la escena
 function onSceneUpdate()
@@ -8,8 +14,8 @@ function onSceneUpdate()
 	move = 0
 
 	if isKeyPressed("LSHIFT") then
-		vel=13
-		des=0.5
+		vel=16
+		des=0.8
 	else
 		vel=12.2
 		des=0.9
@@ -27,15 +33,42 @@ function onSceneUpdate()
 		addCentralForce(Jugador, {0, -vel, 0}, "local")
 	end
 	
-	-- move Sheep
+	-- Moverse hacia delante
 	if isKeyPressed("W") then
 		addCentralForce(Jugador, {0, -vel, 0}, "local")
 	end
 	
-	-- move Sheep
+	-- Moverse hacia detrás
 	if isKeyPressed("S") then
 		addCentralForce(Jugador, {0, vel, 0}, "local")
 	end
-
+	
+	-- Niebla si el usuario toca un lugar prohibido
+	if isCollisionBetween(Jugador, P1) or isCollisionBetween(Jugador, P2) or isCollisionBetween(Jugador, P3) then
+		cont = cont + 1
+		if cont>10 then
+			fot = fot + 1
+			if fot>0 and fot<50 then 
+				setCameraFogDistance(Cam, 50*fot)
+			end
+			enableCameraFog(Cam, true)
+		end
+	else 
+		cont = 0
+		if fot>0 then
+			if fot>50 then fot=50 end
+			setCameraFogDistance(Cam, 50*fot)
+			fot = fot - 1
+		else 
+			enableCameraFog(Cam, false)
+		end	
+		
+	end
+	
+	-- Cambio de color foco
+	seg = os.date("%H")*3600+os.date("%M")*60+os.date("%S")
+	
+	-- print(os.date("%H")*3600+os.date("%M")*60+os.date("%S"))
+	
 	setLinearDamping(Jugador, des)	
 end
